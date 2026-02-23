8b9b8bf2f2290f9bfd8f5f400cb7d0b333e80607
Shaik Mohammad Sajjad <naru07uzu@gmail.com>
2026-02-23 22:42:43 +0530

    Add files via upload
    A fun word based interactive game where users input nouns and verbs to generate a short custom story using formatted string output.

---
+++ madlibs_game.py
@@ -0,0 +1,15 @@
+#madlibs game
+#word game where you create a story 
+#by filling in blanks with random words
+
+noun1 = input("Enter a noun (): ")
+noun2 = input("Enter a noun: ")
+noun3 = input("Enter a noun: ")
+noun4 = input("Enter a noun: ")
+verb = input("Enter a verb: ")
+
+print(f"today i went to the store and i bought a {noun1}")
+print(f"and i went to the {noun2}")
+print(f"and i bought a {noun3}")
+print(f"then i got tired so i {verb}")
+print(f"finally i got home and i went to {noun4}")
