95f8a82e513b2f08dd4a4fdc09bb642f06d47b7e
Shaik Mohammad Sajjad <naru07uzu@gmail.com>
2026-02-24 23:00:36 +0530

    Add files via upload

---
+++ countdown_timer.py
@@ -0,0 +1,12 @@
+import time 
+
+my_time=int(input("Enter the time in seconds: "))
+
+for x in range(my_time, 0, -1):
+    second=x%60
+    minutes=int(x/60)%60
+    hours=int(x/3600)
+    print(f"{hours}:{minutes}:{second}")
+    time.sleep(1)
+
+print("Time's up!")
